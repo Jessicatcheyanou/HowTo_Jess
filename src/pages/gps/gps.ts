e87271115d6c23f81c3ba2857ff81a45259ff67f
jessica kanmegne <jessicatcheyanou@gmail.com>
2018-07-20 22:49:06 +0200

    Added the google Map and displayed it in gps page

--- src/pages/gps/gps.ts
+++ src/pages/gps/gps.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
@@ -8,6 +8,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
+declare var google: any;
 @IonicPage()
 @Component({
   selector: 'page-gps',
@@ -15,11 +16,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class GpsPage {
 
+  map: any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+
+}
 
   ionViewDidLoad() {
+    this.map = new google.maps.Map(document.getElementById('map'), {
+		center: { lat: -34.9011, lng: -56.1645 },
+		zoom: 15
+	});
     console.log('ionViewDidLoad GpsPage');
   }
+
+
 
 }
